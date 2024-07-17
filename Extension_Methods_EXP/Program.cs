8cea008e71cf2c66c5ce8acacc04d543eeca6c81
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-07-17 12:19:36 +0530

    Add project files.

---
+++ Extension_Methods_EXP/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Extension_Methods_EXP;
+
+Console.WriteLine("Hello, World!");
+
+Exp_Extension_Methods obj =
+    new Exp_Extension_Methods();
+obj.callExtensionMethods();
+
+Console.ReadLine();
